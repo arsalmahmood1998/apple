b3a34e90b8abc4a6e1b80eb96b70775e5718da70
arsalmahmood1998 <68080848+arsalmahmood1998@users.noreply.github.com>
2020-12-09 21:27:48 +0500

    code updation

---
+++ js/script.js
@@ -0,0 +1,28 @@
+ $(function() {
+	   
+      // Create the dropdown base
+      $("<select />").appendTo("container sticky");
+      
+      // Create default option "Go to..."
+      $("<option />", {
+         "selected": "selected",
+         "value"   : "",
+         "text"    : "Helloo"
+      }).appendTo("container sticky select");
+      
+      // Populate dropdown with menu items
+      $("container sticky a").each(function() {
+       var el = $(this);
+       $("<option />", {
+           "value"   : el.attr("href"),
+           "text"    : el.text()
+       }).appendTo("container sticky select");
+      });
+      
+	   // To make dropdown actually work
+	   // To make more unobtrusive: http://css-tricks.com/4064-unobtrusive-page-changer/
+      $("container sticky select").change(function() {
+        window.location = $(this).find("option:selected").val();
+      });
+	 
+	 });
